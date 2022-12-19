56320c98b1fce97c3472e6d94a7c06e6b32c4356
Dimitar Petrov <dimitar@nexume.eu>
2022-12-19 16:28:42 +0200

    API 33 update

--- app/src/main/java/com/example/sharedpreferencesexample/MySettingsFragment.java
+++ app/src/main/java/com/example/sharedpreferencesexample/MySettingsFragment.java
@@ -5,8 +5,12 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.CheckBoxPreference;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -14,51 +18,64 @@ import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreferenceCompat;
 
 public class MySettingsFragment extends PreferenceFragmentCompat {
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         final SwitchPreferenceCompat notificationsRef = findPreference("notifications");
         if (notificationsRef != null) {
-            notificationsRef.setSummaryProvider(new Preference.SummaryProvider<SwitchPreferenceCompat>() {
-                @Override
-                public CharSequence provideSummary(SwitchPreferenceCompat preference) {
-                    if (notificationsRef.isChecked()) {
-                        return "Активирани";
-                    } else {
-                        return "Деактивирани";
-                    }
+            notificationsRef.setSummaryProvider((Preference.SummaryProvider<SwitchPreferenceCompat>) preference -> {
+                if (notificationsRef.isChecked()) {
+                    return "Активирани";
+                } else {
+                    return "Деактивирани";
                 }
             });
         }
 
+        final CheckBoxPreference checkBoxPreference = findPreference("scientific_literature");
+        if (checkBoxPreference != null) {
+            checkBoxPreference.setSummaryProvider((Preference.SummaryProvider<CheckBoxPreference>) preference -> {
+                if (checkBoxPreference.isChecked()) {
+                    return "Прочетете интересните книги от света на науката";
+                } else {
+                    return "Интересни книги от света на науката";
+                }
+            });
+        }
+
+
         Preference feedbackPref = findPreference("feedback");
         Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse("https://doctrinalocus.eu/contacts/"));
+        intent.setData(Uri.parse("https://nbu.bg/bg/contacts"));
         if (feedbackPref != null) {
             feedbackPref.setIntent(intent);
         }
 
 
-//        EditTextPreference signaturePreference = findPreference("signature");
-//        if (signaturePreference != null) {
-//            signaturePreference.setSummaryProvider(new Preference.SummaryProvider<EditTextPreference>() {
-//                @Override
-//                public CharSequence provideSummary(EditTextPreference preference) {
-//                    String text = preference.getText();
-//                    if (TextUtils.isEmpty(text)){
-//                        return "Not set";
-//                    }
-//                    return "Length of saved value: " + text.length();
-//                }
-//            });
-//        }
-//
-//        SharedPreferences sharedPreferences =
-//                PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-//        String name = sharedPreferences.getString("signature", "");
-//        Toast.makeText(getActivity().getApplicationContext(), name, Toast.LENGTH_LONG).show();
+        EditTextPreference signaturePreference = findPreference("signature");
+        if (signaturePreference != null) {
+            signaturePreference.setSummaryProvider((Preference.SummaryProvider<EditTextPreference>) preference -> {
+                String text = preference.getText();
+                if (TextUtils.isEmpty(text)) {
+                    return "Not set";
+                }
+                return "Length of saved value: " + text.length();
+            });
+        }
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        prefs.edit().putString("key_name", "SharedPreferences string").apply();
 
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        String name = sharedPreferences.getString("key_name", null);
+        Toast.makeText(getActivity().getApplicationContext(), name, Toast.LENGTH_LONG).show();
+
+        super.onViewCreated(view, savedInstanceState);
     }
 }
